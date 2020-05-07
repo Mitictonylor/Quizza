a7f09544fe0f5f0fc8cbcade3675904ef0dc9208
Antonio Lorusso <mitictonylor@gmail.com>
2020-05-07 10:54:47 +0100

    fix client side

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="">
-    <main-header title='Home' />
+
   </div>
 </template>
 <script>
-import MainHeader from '@/components/MainHeader';
+
 
 export default {
   components: {
-    'main-header': MainHeader
+    
   }
 }
 </script>
