3c88bba941bb8588b4369b0a695cdfbbe413ad05
CrugBarat <craigtbarratt@gmail.com>
2020-05-08 23:49:36 +0100

    adds tile ids with row/column positions
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ client/src/config/TileObjects.js
@@ -0,0 +1,199 @@
+export const TileObjects =  [
+  {
+    id: 'a1',
+    row: 1,
+    column: 1
+  },
+
+  {
+    id: 'a2',
+    row: 1,
+    column: 2
+  },
+
+  {
+    id: 'a3',
+    row: 1,
+    column: 3
+  },
+
+  {
+    id: 'a4',
+    row: 1,
+    column: 4
+  },
+
+  {
+    id: 'a5',
+    row: 1,
+    column: 5
+  },
+
+  {
+    id: 'a6',
+    row: 1,
+    column: 6
+  },
+
+  {
+    id: 'a7',
+    row: 1,
+    column: 7
+  },
+
+  {
+    id: 'b1',
+    row: 2,
+    column: 1
+  },
+
+  {
+    id: 'b4',
+    row: 2,
+    column: 4
+  },
+
+  {
+    id: 'b7',
+    row: 2,
+    column: 7
+  },
+
+  {
+    id: 'c1',
+    row: 3,
+    column: 1
+  },
+
+  {
+    id: 'c4',
+    row: 3,
+    column: 4
+  },
+
+  {
+    id: 'c7',
+    row: 3,
+    column: 7
+  },
+
+  {
+    id: 'd1',
+    row: 4,
+    column: 1
+  },
+
+  {
+    id: 'd2',
+    row: 4,
+    column: 2
+  },
+
+  {
+    id: 'd3',
+    row: 4,
+    column: 3
+  },
+
+  {
+    id: 'd4',
+    row: 4,
+    column: 4
+  },
+
+  {
+    id: 'd5',
+    row: 4,
+    column: 5
+  },
+
+  {
+    id: 'd6',
+    row: 4,
+    column: 6
+  },
+
+  {
+    id: 'd7',
+    row: 4,
+    column: 7
+  },
+
+  {
+    id: 'e1',
+    row: 5,
+    column: 1
+  },
+
+  {
+    id: 'e4',
+    row: 5,
+    column: 4
+  },
+
+  {
+    id: 'e7',
+    row: 5,
+    column: 7
+  },
+
+  {
+    id: 'f1',
+    row: 6,
+    column: 1
+  },
+
+  {
+    id: 'f4',
+    row: 6,
+    column: 4
+  },
+
+  {
+    id: 'f7',
+    row: 6,
+    column: 7
+  },
+
+  {
+    id: 'g1',
+    row: 7,
+    column: 1
+  },
+
+  {
+    id: 'g2',
+    row: 7,
+    column: 2
+  },
+
+  {
+    id: 'g3',
+    row: 7,
+    column: 3
+  },
+
+  {
+    id: 'g4',
+    row: 7,
+    column: 4
+  },
+
+  {
+    id: 'g5',
+    row: 7,
+    column: 5
+  },
+
+  {
+    id: 'g6',
+    row: 7,
+    column: 6
+  },
+
+  {
+    id: 'g7',
+    row: 7,
+    column: 7
+  }
+]
