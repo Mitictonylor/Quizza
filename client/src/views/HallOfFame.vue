68cd6fda61f000fcc1743268a5d940a2f4c38066
CrugBarat <craigtbarratt@gmail.com>
2020-05-12 05:09:13 +0100

    clones home page style to hall of fame
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- client/src/views/HallOfFame.vue
+++ client/src/views/HallOfFame.vue
@@ -1,18 +1,224 @@
-<template lang="html">
-  <div class="">
-    <nav>
-      <router-link :to="{name: 'home'}">Home</router-link>
-      <router-link :to="{name: 'about'}">About</router-link>
-    </nav>
-    <router-view></router-view>
-
+<template>
+  <div id="halloffame" @mousemove="mouseInteraction($event)" v-resize="onResize">
+    <canvas id="canvas"></canvas>
+    <div class="halloffame-container">
+      <div class="halloffame">
+        <div class="menu">
+          <div class="back-container">
+            <router-link  v-on:click="playSound" :to="{name: 'newgame'}"> <img class="back" :src="back"> </router-link>
+          </div>
+          <div class="about-container">
+            <router-link  v-on:click="playSound" :to="{name: 'about'}"> <img class="about" :src="about"> </router-link>
+          </div>
+          <div class="title-container">
+            <p class="title">Hall of Fame</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Circle from '@/helpers/HomePageCanvas.js';
+import resize from 'vue-resize-directive';
+import button from '@/assets/sounds/button.wav';
+import about from '@/assets/images/about.png';
+import back from '@/assets/images/back.png';
+
 export default {
-}
+    name: 'halloffame',
+    data() {
+      return {
+        circleArr: [],
+        mouse: {
+          x: null,
+          y: null
+        },
+        button: button,
+        about: about,
+        back: back
+      }
+    },
+    mounted() {
+      this.setCanvasDimensions();
+      this.circleArray();
+      this.animateCircles();
+    },
+    methods: {
+      setCanvasDimensions() {
+        const canvas = document.querySelector('#canvas')
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+      },
+      circleArray() {
+        const canvas = document.querySelector('#canvas')
+        const c = canvas.getContext('2d');
+        let circleArray = [];
+
+        for (var i = 0; i < 800; i++) {
+          let radius = Math.floor(Math.random() * 6);
+          let x = Math.random() * (innerWidth - radius * 2) + radius;
+          let y = Math.random() * (innerHeight - radius * 2) + radius;
+          let dx = (Math.random() - 0.5) * 3;
+          let dy = (Math.random() - 0.5) * 3;
+          circleArray.push(new Circle (x, y, radius, dx, dy, c, this.mouse))
+        }
+        this.circleArr = circleArray
+        return circleArray
+      },
+      animateCircles() {
+        const canvas = document.querySelector('#canvas')
+        const c = canvas.getContext('2d');
+        requestAnimationFrame(this.animateCircles)
+        c.clearRect(0, 0, innerWidth, innerHeight)
+
+        for (var i = 0; i < this.circleArr.length; i++) {
+          this.circleArr[i].update()
+        }
+      },
+      mouseInteraction(event) {
+        this.mouse.x = event.x;
+        this.mouse.y = event.y;
+      },
+      onResize() {
+        const canvas = document.querySelector('#canvas')
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        this.init()
+      },
+      init() {
+        this.circleArray()
+      },
+      playSound() {
+        var sound = document.getElementById("audio");
+        sound.play();
+      }
+    },
+    directives: {
+      resize,
+    }
+  }
 </script>
 
-<style lang="css" scoped>
+<style scoped>
+  #canvas {
+    background-color: #8e9aaf;
+    position: absolute;
+    left: 0;
+    top: 0 ;
+    z-index: 0;
+  }
+
+  .halloffame-container {
+    width: 100vw;
+    height: 100vh;
+    overflow: auto;
+    display: block;
+    text-align: center;
+    position: absolute;
+    z-index: 1;
+    border-style: solid;
+  }
+
+  .halloffame {
+    width: 50%;
+    height: 70%;
+    display: inline-block;
+    color: white;
+    font-family: 'Russo One', sans-serif;
+    text-shadow: 2px 2px 4px #000000;
+    box-shadow: 2px 2px 4px #000000;
+    -webkit-animation: fadeIn 4s;
+    border-style: solid;
+    border-radius: 5%;
+    margin-top: 50px;
+    background-color: #8e9aaf;
+  }
+
+  @-webkit-keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  #newgame {
+    position: relative;
+  }
+
+  p {
+    padding: 0;
+    margin: 0;
+  }
+
+  .menu {
+    margin-bottom: 50px;
+    margin-top: 20px;
+  }
+
+  .back-container {
+    width: 10%;
+    float: left;
+  }
+
+  .back {
+    height: 60px;
+    border-radius: 50%;
+  }
+
+  .back:hover {
+    background-color: #ff70a6;
+  }
+
+  .back:focus {
+    outline: none;
+  }
+
+  .about-container {
+    width: 10%;
+    float: left;
+  }
+
+  .about {
+    height: 60px;
+  }
+
+  .about:hover {
+    background-color: #907ad6;
+    border-radius: 50%;
+  }
+
+  .about:focus {
+    outline: none;
+  }
+
+  .title-container {
+    width: 78%;
+    float: left;
+  }
+
+  .title {
+    font-family: 'Russo One', sans-serif;
+    font-size: 70px;
+    margin-bottom: 15px;
+    float: left;
+    margin-left: 15%;
+  }
+
+  .sub-text {
+    font-size: 25px;
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  #about {
+    position: relative;
+  }
+
+  p {
+    padding: 0;
+    margin: 0;
+  }
 </style>
